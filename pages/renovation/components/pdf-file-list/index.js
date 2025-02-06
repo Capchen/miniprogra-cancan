7951c87c0f663d522d6ff6f470ee9a19ec2ab233
Capchen <18879890225@163.com>
2025-02-06 12:01:19 +0800

    feat: new doc

--- pages/renovation/components/pdf-file-list/index.js
+++ pages/renovation/components/pdf-file-list/index.js
@@ -2,16 +2,24 @@
 Component({
   data: {
     showLoading: false,
+    wholeHouse: [
+      {
+        title: '电视背景墙柜体',
+        desc: '电视背景墙柜体',
+        url: 'https://pub-af92a95c8fa548e9ba1d0af90192aab9.r2.dev/%E5%85%A8%E5%B1%8B%E5%AE%9A%E5%88%B6%E8%AE%BE%E8%AE%A1%26%E6%8A%A5%E4%BB%B7/1%E7%94%B5%E8%A7%86%E8%83%8C%E6%99%AF%E5%A2%99%E6%9F%9C%E4%BD%93.pdf',
+      },
+    ],
   },
   attached: function () {
   },
   methods: {
-    downPdf() {
+    downPdf(e) {
       this.setData({
         showLoading: true,
       })
+      const item = e.currentTarget.dataset.item
       wx.downloadFile({
-        url: 'https://pub-af92a95c8fa548e9ba1d0af90192aab9.r2.dev/%E8%A3%85%E4%BF%AE%E5%8F%82%E8%80%83.pdf',
+        url: item.url,
         success: function (res) {
           const filePath = res.tempFilePath
           wx.openDocument({
@@ -32,11 +40,6 @@ Component({
             showLoading: false,
           })
         },
-      })
-    },
-    navigatorPdfList() {
-      wx.navigateTo({
-        url: '/pages/renovation/components/pdf-file-list/index',
       })
     }
   },
